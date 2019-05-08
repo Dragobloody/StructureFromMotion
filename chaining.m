098e222236c1ebc96ea2e574604a977bc038f81e
sergicastellasape <castella.sergi@gmail.com>
2019-05-08 10:51:42 +0200

    additions of threshold and rounding to the chaining function

--- chaining.m
+++ chaining.m
@@ -1,9 +1,8 @@
-function PointViewMatrix = chaining(images, path)
+function PointViewMatrix = chaining(images, path, bg_threshold, rounding)
     % images = dir(strcat(path, '/' , '*.png')) ;
     % images is a struct 
     img1_name = images(1).name ;
     img1 = imread(strcat(path, '/', img1_name)) ;
-    bg_threshold = 100 ;
     f_row = 1 ;
     %f = waitbar(length(images), 'Progress...')
     
@@ -12,35 +11,36 @@ function PointViewMatrix = chaining(images, path)
         img2 = imread(strcat(path, '/', img2_name)) ;
         [keypoints1, keypoints2, scores] = keypoint_matching(img1, img2, bg_threshold) ; % what value to threshold?
         
-        %[scores_sorted, scores_order] = sort(scores) ;
-        %keypoints1 = keypoints1(:,scores_order) ;
-        %keypoints2 = keypoints2(:,scores_order) ;
+        %Tried sorting to only get the first X best matches
+        [scores_sorted, scores_order] = sort(scores) ;
+        %Rounding to the nearest rounding (i.e. if it's 5: 183->185)
+        keypoints1 = round(keypoints1(:,scores_order)/rounding, -1)*rounding ;
+        keypoints2 = round(keypoints2(:,scores_order)/rounding, -1)*rounding ;
         
-        %N = 20 ;
-        %keypoints1 = keypoints1(:,1:N) ;
-        %keypoints2 = keypoints2(:,1:N) ;
-        
-        if f_row == 1
+        N = 20 ;
+        if size(keypoints1, 2) > N
+            keypoints1 = keypoints1(:,1:N) ;
+            keypoints2 = keypoints2(:,1:N) ;
+           
+            
+        if f_row == 1 % only for the first iteration
             PVM(f_row:f_row+1,:) = keypoints1 ;
             PVM(f_row+2:f_row+3,:) = keypoints2 ;
             f_row = f_row + 2; % first time we'll jump 4 rows for this counter
-            
+        
+        % Fill the PVM matrix with matched and new columns
         else
+            % check matching points already in PVM 
             [C, i_PVM, i_kp] = intersect(PVM(f_row-2:f_row-1, :)', keypoints1', 'rows') ;
-            
             % Introduce points that were already in columns
             PVM(f_row:f_row+1, i_PVM) = keypoints2(:, i_kp) ;
-            
             % add points to new points to new columns from previous and frames:
             not_matched_idx = setdiff(1:size(keypoints1, 2), i_kp) ;
-         
             % Initialize columns for unmatched point pairs
             new_columns = zeros(f_row+1, length(not_matched_idx)) ;
-
             % fill values for new points
             new_columns(f_row-2:f_row-1,:) = keypoints1(:, not_matched_idx) ;
             new_columns(f_row:f_row+1,:) = keypoints2(:, not_matched_idx) ;
-            
             % concatenate with the point view matrix
             PVM = [PVM new_columns] ;
 
--- main_5.m
+++ main_5.m
@@ -1,48 +1,63 @@
-% Read PointViewMatrix.txt into D
+%% Use the provided PointViewMatrix.txt
 D = importdata('Data/PointViewMatrix.txt') ;
+% construct the motion and structure matrices from datapoints D
 [M, S] = structure_from_motion(D) ;
 
-%%
+%% Create our own D matrix with consecutive chaining 
 IMAGES = dir('Data/House/*.png') ;
 path = 'Data/House' ;
-D = chaining(IMAGES, path) ;
+bg_threshold = 50 ;
+% round to the nearest "5" the point coordinates of matching
+rounding = 10 ; 
+D = chaining(IMAGES, path, bg_threshold, rounding) ; % Function for part 4
+% visualize PVM
+spy(D)
+% derive the structure and motion matrices from datapoints D
+[M, S] = structure_from_motion(D) ;
 
+%% Iterative procedure: section 5.4
+f = waitbar(0, 'progress') ;
 
-%%
-
+n_stitching = 3 ;
+bg_threshold = 10 ;
+rounding = 1 ;
 path = 'Data/House' ;
 IMAGES = dir(strcat(path, '/*.png')) ;
-n_stitching = 3 ;
 S = [] ;
-f = waitbar(0, 'progress') ;
 
 for j = 1:length(IMAGES)-n_stitching
     waitbar(j/length(IMAGES)) ;
     
-    images = IMAGES(j:j+3) ;
-    D = chaining(images, path) ;
+    % Get the n_stitching images from which to create the Point View Matrix (3 or 4)
+    images = IMAGES(j:j+3) ; 
+    D = chaining(images, path, bg_threshold, rounding) ;
     [M_new, S_new] = structure_from_motion(D) ;
-    
     
     if j == 1
         S = S_new ;
         S_last = S_new ;
     else
+        % apply padding to the smallest matrix and calculate the
+        % transformation from the last set of points. Not all "historical"
+        % points are used bc the algorithm gets very slow otherwise
         if size(S_last,2) > size(S_new, 2)
-            S_new_pad = padarray(S_new, size(S_last), 0) ;
-            [d, S_new_trans, tr] = procrustes(S_last, S_new) ;
-        else size(S_last,2) <= size(S_new, 2)
-            S_last_pad = padarray(S_last, size(S_new), 0) ;
-            [d, S_new_trans, tr] = procrustes(S_last, S_new) ;
+            dif = size(S_last,2) - size(S_new, 2) ;
+            S_new_pad = [S_new zeros(3, dif)] ;
+            [d, S_new_trans, tr] = procrustes(S_last, S_new_pad) ;
+        else size(S_last,2) <= size(S_new, 2) 
+            dif = size(S_new, 2)-size(S_last,2) ;
+            S_last_pad = [S_last zeros(3, dif)] ;
+            [d, S_new_trans, tr] = procrustes(S_last_pad, S_new) ;
         end
+        % Append new points from the n_stitching 
         S = [S S_new_trans] ;
         S_last = S_new_trans ;
     end
-    
 end
 close(f)
 
-%%
+%% Plotting results for the reconstruction
+
 X = S(1,:) ;
 Y = S(2,:) ;
 Z = S(3,:) ;
