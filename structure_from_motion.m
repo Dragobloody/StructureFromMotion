08437ab75e18331e3f30b03312929a1ff6ee63f3
sergicastellasape <castella.sergi@gmail.com>
2019-05-09 21:23:37 +0200

    Updates to main_5, still unsuccessful

--- structure_from_motion.m
+++ structure_from_motion.m
@@ -3,19 +3,34 @@ function [M, S] = structure_from_motion(D)
 % is a point
 
 % Normalize
-D = D - mean(D')' ;
+D = D - mean(D,2) ;
 
 % Apply SVD
 [U, W, V] = svd(D) ;
 
+
 % Reduce to rank 3
 W_3 = W(1:3, 1:3) ;
 U_3 = U(:,1:3) ;
-V_3 = V(:, 1:3) ;
-
+V_3 = V(:,1:3) ;
+norm(D-U*W*V')
+norm(D-U_3*W_3*V_3')
 % Decomposition of Motion and Structure (M and S)that minimizes abs((D-MS)^2)
-M = U_3 * sqrtm(W_3) ;
-S = sqrtm(W_3) * V_3' ;
+M = U_3 * sqrt(W_3) ;
+S = sqrt(W_3) * V_3' ;
+norm(D-M*S)
 
+% Eliminate affine ambiguity
+% A_i * L * A_i = Id
+% L = C * C'
+% M = M * C
+% S = inv(C) * S
+
+L = pinv(M)*pinv(M)' ;
+C = chol(L);
+
+M = M*C;
+S = inv(C)*S;
+norm(D-M*S)
 end
 
